90afd0ee918334cac30aaf513896181ba199fa30
Ruzibiza kelia <159281239+ruzibizak@users.noreply.github.com>
2026-04-15 20:28:07 +0200

    feat: add solution for Question2

---
+++ Question2.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+class Pair<A,B> {
+    public final A first;
+    public final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        Pair<String, Integer> p = new Pair<>("age", 20);
+        String key = p.first;
+        var value = p.second;
+        System.out.println(key);
+        System.out.println(value);
+    }
+}
